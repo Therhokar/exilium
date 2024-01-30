73bb3141d26cc6f1f834e52c8840e04d57e8146d
Therhokar <therhokar@gmail.com>
2024-01-30 17:38:19 +0800

    修复武器特质显示，补充属性图标，调整样式等

--- assets/SiteNotice-8cad3aa7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-edc71348.js";const t={};function n(r,s){return c(),o("p",null,"注意：本站目前显示的是公测版本1.0.2076.2271.3523的信息。")}const a=e(t,[["render",n]]);export{a as S};
