7e1bc30c124a2090577c0eccb1f062c470ffab16
Therhokar <therhokar@gmail.com>
2024-02-06 22:49:15 +0800

    修正敌方技能显示，修正纵火者技能图标；敌方列表添加筛选大型单位选项

--- assets/SiteNotice-863c359d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-b43b8422.js";const t={};function n(r,s){return c(),o("p",null,"注意：本站目前显示的是公测版本1.0.2010.2339.3522的信息。")}const a=e(t,[["render",n]]);export{a as S};
