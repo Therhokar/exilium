ec570a2f19d08d37b3f3c5e32399bdfbdd5f0db0
Therhokar <therhokar@gmail.com>
2024-03-13 02:47:06 +0800

    添加角色列表筛选，调整武器和敌方列表筛选；添加角色关联武器；调整排版和样式；资源版本1.2010.2656.3872

--- assets/SiteNotice-47c21966.js
+++
@@ -1,1 +0,0 @@
-import{o,c as t,i as e,g as c,z as r}from"./index-5e8abe1e.js";const p={__name:"SiteNotice",setup(a){return(s,n)=>(o(),t("p",null,[e("注意：本站目前显示的是公测版本"),c(r),e("的信息。")]))}};export{p as _};
